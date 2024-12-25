94f91dd2a9b4f0d6e8495d90b51fcf1c722bb15a
princekop <154677796+princekop@users.noreply.github.com>
2024-12-25 10:30:37 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Darkhosting Paymenter Installer
+# Made by Nikhil
+
+# Check if running as root
+if [ "$EUID" -ne 0 ]; then 
+    echo "Please run as root"
+    exit 1
+fi
+
+# Download the main script
+curl -o darkhosting-paymenter-installer.sh https://raw.githubusercontent.com/princekop/darkhosting-paymenter-installer/main/darkhosting-paymenter-installer.sh
+
+# Make it executable
+chmod +x darkhosting-paymenter-installer.sh
+
+# Run the script
+./darkhosting-paymenter-installer.sh
+
+# Clean up
+rm darkhosting-paymenter-installer.sh
